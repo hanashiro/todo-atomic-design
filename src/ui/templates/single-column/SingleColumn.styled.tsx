66b4578f8554f89160d8c1e0ce827b609f4d8fb9
hanashiro <felipeaah@gmail.com>
2024-02-01 13:10:21 -0300

    fix: :lipstick: improved the behavior of containers height

--- src/ui/templates/single-column/SingleColumn.styled.tsx
+++ src/ui/templates/single-column/SingleColumn.styled.tsx
@@ -1,7 +1,7 @@
 import { styled } from '@mui/material';
 
 export const PageWrapper = styled('div')`
-    height: 100%;
+    min-height: 100vh;
     display: flex;
     flex-direction: column;
     background-color: ${({ theme }) => theme.palette.background.default};
